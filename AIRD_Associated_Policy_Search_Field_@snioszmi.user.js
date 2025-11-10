8cfaf459f41f83f92e46a4d1185c3af8b143d4a0
snioszmi <michalsnioszek15@gmail.com>
2025-11-10 12:52:46 +0100

    Update AIRD_Associated_Policy_Search_Field_@snioszmi.user.js

--- AIRD_Associated_Policy_Search_Field_@snioszmi.user.js
+++ AIRD_Associated_Policy_Search_Field_@snioszmi.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AIRD Associated Policy Search Field @snioszmi
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.4
 // @description  Wyszukuje podobne słowa w polu wyboru Associated Policy na stronie Content Risk Engine
 // @match        https://content-risk-engine-iad.iad.proxy.amazon.com/experiments/update/*
 // @match        https://content-risk-engine-iad.iad.proxy.amazon.com/experiments/create*
@@ -39,7 +39,7 @@
         floatingContainer.style.cssText = `
             position: absolute;
             right: 15px;
-            top: 670px;
+            top: 750px;
             background-color: #fff;
             border: 1px solid #ddd;
             border-radius: 5px;
